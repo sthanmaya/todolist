98954c75a393be872f471f13bc9de12df10f663f
sthanmaya <140145468+sthanmaya@users.noreply.github.com>
2024-06-06 12:45:03 +0530

    Main Files Uploaded

---
+++ src/components/Todo.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import TodoForm from "./TodoForm";
+import TodoList from "./TodoList";
+
+function Todo() {
+  const [todos, setTodos] = useState([]);
+
+  //to add items 
+  const addTodo = (todo) => {
+    const newTodos = [todo, ...todos];
+    setTodos(newTodos);
+    console.log(...todos);
+  };
+
+  //to update items 
+  const updateTodo = (todoId, newValue) => {
+    setTodos((prev) =>
+      prev.map((item) => (item.id === todoId ? newValue : item))
+    );
+  };
+
+  //to remove items 
+  const removeTodo = (id) => {
+    const removedArr = [...todos].filter((todo) => todo.id !== id);
+
+    setTodos(removedArr);
+  };
+
+  //to show items as completed 
+  const completeTodo = (id) => {
+    let updatedTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        todo.isComplete = !todo.isComplete;
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  };
+
+  return (
+    <div>
+      <h1 className="header">Add your Plan for Today?</h1>
+
+      {/*once we display header to the webpage 
+      we will pass the function as props to TodoForm and TodoList to display the data to user*/}
+
+      <TodoForm onSubmit={addTodo} />
+      <TodoList
+        todos={todos}
+        updateTodo={updateTodo}
+        removeTodo={removeTodo}
+        completeTodo={completeTodo}
+      />
+    </div>
+  );
+}
+
+export default Todo;
